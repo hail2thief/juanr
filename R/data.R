402286f23c5e7b1b53041774f5fad0d4db8ea4d3
Juan Tellez <juan.f.tellez@gmail.com>
2020-05-25 13:37:10 -0400

    ugh, still travis problems

--- R/data.R
+++ R/data.R
@@ -1,23 +1,24 @@
-#' Latin America and the Caribbean World Bank Informality Data
+#' World Bank Informality Data
 #'
 #' A dataset containing data from World Bank indicators on urbanization and
-#' informality around the world. The variables are as follows:
+#' informality in select Latin America and the Caribbean countries. The variables are as follows:
 #'
 #' \itemize{
 #' \item country. Country name.
 #' \item year. Year.
-#' \item pct_pop_slum. Population living in slums (% of urban population).
-#' \item pct_informal_work. Informal employment (% of total non-agricultural employment).
-#' \item pct_urban. Urban population (% of total population).
+#' \item pct_pop_slum. Population living in slums (\% of urban population).
+#' \item pct_informal_work. Informal employment (\% of total non-agricultural employment).
+#' \item pct_urban. Urban population (\% of total population).
 #' \item pct_firms_compete. Percent of firms competing against unregistered or informal firms.
 #' \item pct_firms_constrain. Percent of firms identifying practices competitors in the informal sector as a major constraint.
 #' \item pct_firms_registered. Percent of firms formally registered when they started operations in the country.
-#' \item pct_largest_city. Population in the largest city (% of urban population).}
+#' \item pct_largest_city. Population in the largest city (\% of urban population).
+#' }
 #'
 #' @docType data
 #' @keywords datasets
 #' @name informal
 #' @usage data(informal)
 #' @format A (tibble) data frame with 1182 rows and 9 variables.
-#' @source World Bank (wbstats package).
+#' @source wbstats package and World Bank Indicators.
 "informal"
--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 r package
 
 <!-- badges: start -->
-[![Travis build status](https://travis-ci.org/hail2thief/ladata.svg?branch=master)](https://travis-ci.org/github/hail2thief/ladata)
+[![Travis build status](https://travis-ci.org/hail2thief/ladata.svg?branch=master)](https://travis-ci.org/hail2thief/ladata)
 <!-- badges: end -->
