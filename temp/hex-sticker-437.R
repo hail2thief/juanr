878a48a186e0dbe74e2d432753079c52ffa033d4
Juan Tellez <juan.f.tellez@gmail.com>
2020-09-01 12:10:26 -0400

    not sure what happened here, looks like a hex-sticker edit

--- temp/hex-sticker-437.R
+++ temp/hex-sticker-437.R
@@ -13,8 +13,8 @@ mao = png::readPNG('temp/mexico.png')
 mao = grid::rasterGrob(mao, interpolate = TRUE)
 
 sticker(mao, package = "ladata",
-        p_size = 8,
-        p_y = .45, p_color = "#482475", p_family = "roboto",
+        p_size = 9,
+        p_y = .4, p_color = "#482475", p_family = "roboto",
         s_x = 1, s_y = 1.2, white_around_sticker = TRUE,
         s_width = 3, s_height = 1.8,spotlight = FALSE,
         h_fill = "white",h_color = "#482475",
